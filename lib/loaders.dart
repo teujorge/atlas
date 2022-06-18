45fcef1edb558ec6b443723edda7b0cc443f8925
Matheus <mrljorge@outlook.com>
2022-06-18 15:46:09 -0300

    removed unused code

--- lib/loaders.dart
+++ lib/loaders.dart
@@ -4,53 +4,7 @@ import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
 
 import 'main.dart';
-import 'characters/enemy.dart';
 import 'characters/obstacle.dart';
-import 'collectables/baked_good.dart';
-
-void addBakedGoods(TiledComponent homeMap, AtlasGame game) async {
-  final bakedGoodsGroup = homeMap.tileMap.getLayer<ObjectGroup>("BakedGoods");
-
-  for (var bakedGood in bakedGoodsGroup!.objects) {
-    String? bakedGoodFile;
-    switch (bakedGood.type) {
-      case 'ApplePie':
-        bakedGoodFile = "apple_pie.png";
-        break;
-      case 'Cookie':
-        bakedGoodFile = "cookie.png";
-        break;
-      case 'CheeseCake':
-        bakedGoodFile = "cheesecake.png";
-        break;
-      case 'ChocoCake':
-        bakedGoodFile = "choco_cake.png";
-        break;
-    }
-
-    if (bakedGoodFile != null) {
-      game.add(
-        BakedGoodComponent()
-          ..position = Vector2(bakedGood.x, bakedGood.y)
-          ..width = bakedGood.width
-          ..height = bakedGood.height
-          ..sprite = await game.loadSprite(bakedGoodFile),
-      );
-    }
-  }
-}
-
-void addFriends(TiledComponent homeMap, AtlasGame game) {
-  final friendGroup = homeMap.tileMap.getLayer<ObjectGroup>("Friends");
-  for (var friendBox in friendGroup!.objects) {
-    game.add(
-      EnemyCharacter()
-        ..position = Vector2(friendBox.x, friendBox.y)
-        ..width = friendBox.width
-        ..height = friendBox.height,
-    );
-  }
-}
 
 void addObstacles(TiledComponent homeMap, AtlasGame game) {
   final obstacleGroup = homeMap.tileMap.getLayer<ObjectGroup>("Obstacles");
