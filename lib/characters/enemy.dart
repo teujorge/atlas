0a5a2a4d4f23708a4337867564428c955b5f00f6
Matheus <mrljorge@outlook.com>
2022-06-17 21:06:21 -0300

    enemy random spawn vector for position and vel

--- lib/characters/enemy.dart
+++ lib/characters/enemy.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/sprite.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
@@ -7,6 +9,7 @@ import '../main.dart';
 
 class EnemyCharacter extends SpriteAnimationComponent
     with CollisionCallbacks, HasGameRef<AtlasGame> {
+  late Vector2 randomMove;
   final double animationSpeed = .3;
 
   EnemyCharacter() {
@@ -14,10 +17,30 @@ class EnemyCharacter extends SpriteAnimationComponent
     add(RectangleHitbox());
   }
 
+  Vector2 getRandomVector({required max, min = 0, onlyPos = true}) {
+    Vector2 randomVector = Vector2(
+      Random().nextDouble() * max,
+      Random().nextDouble() * max,
+    );
+
+    if (!onlyPos) {
+      if (Random().nextInt(1) == 0) {
+        randomVector.x *= -1;
+      }
+      if (Random().nextInt(1) == 0) {
+        randomVector.y *= -1;
+      }
+    }
+
+    return randomVector;
+  }
+
   @override
   Future<void>? onLoad() async {
     await super.onLoad();
-    position = Vector2(200, 200);
+    position = getRandomVector(max: 400, min: 100);
+    randomMove = getRandomVector(max: 50, onlyPos: false);
+
     size = Vector2(50, 50);
 
     animation = await gameRef.loadSpriteAnimation(
@@ -43,6 +66,20 @@ class EnemyCharacter extends SpriteAnimationComponent
   @override
   void update(double dt) {
     super.update(dt);
-    position.add(Vector2(2, 1) * 10 * dt);
+    position.add(randomMove * dt);
+
+    // switch x vel
+    if (position.x - 25 > gameRef.mapWidth) {
+      randomMove.x *= -1;
+    } else if (position.x < 0) {
+      randomMove.x *= -1;
+    }
+
+    // switch y vel
+    if (position.y - 25 > gameRef.mapHeight) {
+      randomMove.y *= -1;
+    } else if (position.y < 0) {
+      randomMove.y *= -1;
+    }
   }
 }
