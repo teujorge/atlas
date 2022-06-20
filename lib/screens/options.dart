789481a279df3c0fe39fab34a77d60811138d535
Matheus <mrljorge@outlook.com>
2022-06-19 22:30:54 -0300

    options screen

---
+++ lib/screens/options.dart
@@ -0,0 +1,55 @@
+import 'package:Atlas/screens/menu.dart';
+import 'package:flutter/material.dart';
+import 'package:flame/game.dart';
+
+class Options extends StatelessWidget {
+  const Options({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 50.0),
+              child: Text(
+                "Settings",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 20.0,
+                      color: Color.fromARGB(199, 255, 255, 255),
+                      offset: Offset(0, 0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //navigation to options button
+                Navigator.of(context).pop();
+              },
+              child: const Text("Resume"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                //navigation to options button
+                Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                    builder: (context) => const MainMenu(),
+                  ),
+                  (route) => route.isFirst,
+                );
+              },
+              child: const Text("Quit"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
