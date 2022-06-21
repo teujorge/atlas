51e564fe8c6fd8bc4b545b68da917c4e320e8575
Davi Jorge <guimell11@hotmail.com>
2022-06-21 17:31:54 -0300

    add a background image to the main menu

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -9,39 +9,47 @@ class MainMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 50.0),
-              child: Text(
-                "Atlas Arena",
-                style: TextStyle(
-                  fontSize: 50.0,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 20.0,
-                      color: Color.fromARGB(199, 255, 255, 255),
-                      offset: Offset(0, 0),
-                    ),
-                  ],
+        child: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/backgroundCastel.png"),
+                fit: BoxFit.cover),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Text(
+                  "Atlas Arena",
+                  style: TextStyle(
+                    fontSize: 50.0,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 20.0,
+                        color: Color.fromARGB(199, 255, 255, 255),
+                        offset: Offset(0, 0),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => CharacterSelection(),
-                    ),
-                  );
-                },
-                child: const Text("Play"),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => CharacterSelection(),
+                      ),
+                    );
+                  },
+                  child: const Text("Play"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
