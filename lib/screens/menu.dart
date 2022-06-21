2c23ebd1ebe588511ee0eaa6c623393c2f0de886
Matheus <mrljorge@outlook.com>
2022-06-20 23:45:10 -0300

    filled character select screen (temp)

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -34,7 +34,7 @@ class MainMenu extends StatelessWidget {
                 onPressed: () {
                   Navigator.of(context).push(
                     MaterialPageRoute(
-                      builder: (context) => const CharacterSelection(),
+                      builder: (context) => CharacterSelection(),
                     ),
                   );
                 },
