bcd99d19efe7bae7cbed950137467d0fec5b9065
Matheus Jorge <mrljorge@outlook.com>
2023-03-08 14:47:24 -0300

    creating game requires character enum

--- lib/screens/play.dart
+++ lib/screens/play.dart
@@ -5,27 +5,12 @@ import '../main.dart';
 import '../loaders.dart';
 
 class GamePlay extends StatelessWidget {
-  late final CharName character;
-  GamePlay({Key? key, required int character}) : super(key: key) {
-    switch (character) {
-      case 0:
-        this.character = CharName.mage;
-        break;
-      case 1:
-        this.character = CharName.archer;
-        break;
-      case 2:
-        this.character = CharName.knight;
-        break;
-      default:
-        this.character = CharName.mage;
-        break;
-    }
-  }
+  final CharName character;
+  const GamePlay({Key? key, required this.character}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    AtlasGame _atlasGame = AtlasGame(context, character);
-    return GameWidget(game: _atlasGame);
+    AtlasGame atlasGame = AtlasGame(context, character);
+    return GameWidget(game: atlasGame);
   }
 }
